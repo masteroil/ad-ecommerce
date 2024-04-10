eb07ad6f16e0cbbed709df6fb5c06bd7e0a8895b
aizem_blaze <aizem@blaze.online>
2024-04-10 08:48:36 +0800

    change pc

---
+++ src/app/(pages)/products/Filters/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Category } from '../../../../payload/payload-types'
+import classes from './index.module.scss'
+const Filters = ({ categories }: { categories: Category[] }) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Filters
